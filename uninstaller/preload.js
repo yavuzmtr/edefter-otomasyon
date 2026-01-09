80ea7f3ec092768b7767d74133208148ac16e928
yavuzmtr <yavuzmercimek@gmail.com>
2026-01-09 18:25:48 +0300

    uninstaller klasörü silindi (NSIS installer kullanılıyor)
    - uninstaller/ klasörü tamamen kaldırıldı
    - NSIS installer zaten gerekli kaldırıcı fonksiyonu sağlıyor
    - scripts/installer.nsi ve installer.nsh var

--- uninstaller/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('uninstallerAPI', {
-  uninstall: (full) => ipcRenderer.invoke('uninstall', { full })
-});
